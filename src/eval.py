fbab185a4a0b955c306d5c09ae850cf759e60876
Sohaib LAFIFI <sohaib.lafifi@univ-artois.fr>
2023-04-23 21:15:54 +0200

    feat: update eval

--- src/eval.py
+++ src/eval.py
@@ -3,11 +3,12 @@ import torch
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
 from transformers import FlaubertTokenizer, FlaubertForSequenceClassification
 
 # Read data from multiple Excel files
-years = [2022]  # range(2015, 2023)  # Update the range according to your data
-texts, scores = [], []
+years = [2023]  # range(2015, 2023)  # Update the range according to your data
+texts, scores, codes  = [], [], []
 
 for year in years:
     excel_file = f'data/raw/applications_{year}.xlsx'
@@ -16,6 +17,7 @@ for year in years:
     # Assume the Excel file has two columns: 'motivations' and 'score'
     texts.extend(df['motivations'].tolist())
     scores.extend(df['score'].tolist())
+    codes.extend(df['code'].tolist())
 
 # Pre-process the data
 # Choose among ['flaubert/flaubert_small_cased', 'flaubert/flaubert_base_uncased',
@@ -24,17 +26,16 @@ modelname = 'flaubert/flaubert_base_uncased'
 
 tokenizer = FlaubertTokenizer.from_pretrained(modelname, do_lowercase=False)
 
-# Train-test split
-train_texts, test_texts, train_scores, test_scores = train_test_split(texts, scores, test_size=0.2, random_state=42)
 
 # Encode the texts
-test_encoded_data = tokenizer([str(text) for text in test_texts], padding=True, truncation=True, return_tensors="pt")
+test_encoded_data = tokenizer([str(text) for text in texts], padding=True, truncation=True, return_tensors="pt")
 
 
 class ApplicationDataset(Dataset):
-    def __init__(self, encoded_data, scores):
+    def __init__(self, encoded_data, scores, codes):
         self.encoded_data = encoded_data
         self.scores = scores
+        self.codes = codes
 
     def __len__(self):
         return len(self.scores)
@@ -42,10 +43,11 @@ class ApplicationDataset(Dataset):
     def __getitem__(self, idx):
         item = {key: val[idx].clone().detach() for key, val in self.encoded_data.items()}
         item['labels'] = torch.tensor(self.scores[idx], dtype=torch.float)
+        item['codes'] = torch.tensor(self.codes[idx], dtype=torch.int32)
         return item
 
 
-test_dataset = ApplicationDataset(test_encoded_data, test_scores)
+test_dataset = ApplicationDataset(test_encoded_data, scores, codes)
 
 # Model and DataLoader
 mps_available = hasattr(torch.backends, "mps") and torch.backends.mps.is_available()
@@ -61,15 +63,18 @@ model.to(device)
 
 # Evaluation
 model.eval()
-predictions, true_labels = [], []
+predictions = []
 test_loader = DataLoader(test_dataset, batch_size=8)
+results = {}
 with torch.no_grad():
-    for batch in test_loader:
-        inputs = {key: val.to(device) for key, val in batch.items() if key != 'labels'}
-        labels = batch['labels'].to(device)
+    for batch in tqdm(test_loader):
+        print(batch.keys())
+        inputs = {key: val.to(device) for key, val in batch.items() if key != 'labels' and key != 'codes'}
         outputs = model(**inputs)
-        predictions.extend(outputs.logits.squeeze(-1).tolist())
-        true_labels.extend(labels.tolist())
+        codes = batch['codes']
+        results.update({code: prediction for code, prediction in zip(codes.tolist(), outputs.logits.squeeze(-1).tolist())})
 
-mse = mean_squared_error(true_labels, predictions)
-print(f"Mean Squared Error: {mse}")
+    for code, score in results.items() :
+        print('{} => {}'.format(code, score))
+# ...
+
